bba4a579cff599cf5ca42685b366d8d89131e51b
Sean Gallagher <sgallagr@vt.edu>
2018-09-28 16:26:19 -0400

    Fixed spacing issue

--- expression.hpp
+++ expression.hpp
@@ -96,7 +96,7 @@ private:
   Expression handle_lookup(const Atom & head, const Environment & env);
   Expression handle_define(Environment & env);
   Expression handle_begin(Environment & env);
-	Expression handle_lambda(Environment & env);
+  Expression handle_lambda(Environment & env);
 };
 
 /// Render expression to output stream
