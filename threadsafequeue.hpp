609b8a7216fe24fdb513760158946be15d728fff
Sean Gallagher <sgallagr@vt.edu>
2018-12-01 23:05:42 -0500

    Moved thread safe queue definitions into header file

--- threadsafequeue.hpp
+++ threadsafequeue.hpp
@@ -9,13 +9,34 @@ template<typename T>
 class ThreadSafeQueue {
 public:
   
-  void push(const T & value);
+  void push(const T & value) {
+    std::unique_lock<std::mutex> lock(the_mutex);
+    the_queue.push(value);
+    lock.unlock();
+    the_condition_variable.notify_one();
+  }
 
-  bool empty() const;
+  bool empty() const {
+    std::lock_guard<std::mutex> lock(the_mutex);
+    return the_queue.empty();
+  }
 
-  bool try_pop(T& popped_value);
+  bool try_pop(T& popped_value) {
+    std::lock_guard<std::mutex> lock(the_mutex);
+    if (the_queue.empty()) return false;
 
-  void wait_and_pop(T& popped_value);
+    popped_value = the_queue.front();
+    the_queue.pop();
+    return true;
+  }
+
+  void wait_and_pop(T& popped_value) {
+    std::unique_lock<std::mutex> lock(the_mutex);
+    while (the_queue.empty()) the_condition_variable.wait(lock);
+
+    popped_value = the_queue.front();
+    the_queue.pop();
+  }
 
 private:
 
